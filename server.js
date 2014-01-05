bcfa79c0a66c35779e368a2aea907c68b88fe88c
flaky <flakyfilibuster@gmail.com>
2014-01-05 17:36:21 +0100

    add favicon to serverlogic

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ var GBOOKAPI = 'https://www.googleapis.com/books/v1/volumes?q=isbn:';
 
 // define path to folder for serving static files
 app.use(express.static(__dirname + '/public'));
+app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
 
 // middleware to read form post values
 app.use(express.multipart());
