732714df49f6948cf783840e9e50cd7ac9f615b5
flaky filibuster <flakyfilibuster@gmail.com>
2014-01-07 21:35:15 +0100

    at last, replaced express.multipart with express.json, no more annoying warning messages

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
 
 // middleware to read form post values
-app.use(express.multipart());
+app.use(express.json());
 
 // ROOT logic
 app.get('/', function(req, res){
