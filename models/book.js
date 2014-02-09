03de7f0d986dbebafaf0367efb3dc0c896d27819
flaky <flakyfilibuster@gmail.com>
2014-02-09 18:15:41 +0100

    add Book constructor | Book.Key static method to define current environment | Book.getAll to retreive all books from DB | Book.protoype.save to save instance in BD

--- models/book.js
+++ models/book.js
@@ -1,7 +1,40 @@
+var db = require('mongojs').connect('localhost/mydb');
+
 var Book = (function() {
+
+    function Book(attributes) {
+        var key, value;
+        for (key in attributes) {
+            value = attributes[key];
+            this[key] = value;
+        }
+    }
+
+    Book.key = function() {
+        return 'book:'+ process.env.NODE_ENV;
+    };
+
+    Book.getAll = function(callback) {
+        db.collection(Book.key()).find().sort({$natural: -1}, function(err, objects) {
+            if(!err) {
+                var allBooks = [];
+                for(var i = 0, len = objects.length; i<len; i++) {
+                    allBooks.push(new Book(objects[i]));
+                }
+                callback(err, allBooks);
+            }
+        });
+    };
+
+    Book.prototype.save = function(callback) {
+        db.collection(Book.key()).save(this, function(err, code) {
+            callback(null, this);
+        });
+    };
+
 
     return Book;
 
-})(db);
+})();
 
 module.exports = Book;
