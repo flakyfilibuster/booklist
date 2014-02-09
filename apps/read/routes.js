f14c84dd0204ecf7c51340e34ea113f8d947677e
flaky <flakyfilibuster@gmail.com>
2014-02-09 19:43:23 +0100

    separate number of books from title

--- apps/read/routes.js
+++ apps/read/routes.js
@@ -4,7 +4,8 @@ var routes = function(app, db){
     app.get('/', function(req, res){
         Book.getAll(function(err, books) {
             res.render(__dirname + "/views/index", {
-                title: "Completed Books",
+                amount: books.length,
+                title: " Completed Books",
                 books: books,
                 stylesheet1: "main",
                 stylesheet2: "bootstrap"
