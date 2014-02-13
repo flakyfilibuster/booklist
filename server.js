aee019f3336482ec9bffdf7715c3f2ae96476825
flaky <flakyfilibuster@gmail.com>
2014-02-13 17:29:23 +0100

    documentation and refactoring + added a helper

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 #!/bin/env node
 
-//*********************************************
+// ============================================
 // MODULE DEPENDENCIES
-// ********************************************
-
+// ============================================
 var express     = require('express');
 var app         = module.exports = express();
 var request     = require('request');
-var MongoStore  = require('connect-mongo')(express);
+var MongoStore = require('connect-mongo')(express);
+var passport    = require('passport');
+var flash       = require('connect-flash');
 
-//*********************************************
-// APP CONFIGURATION
-// ********************************************
 
+// ============================================
+// APP CONFIGURATION - DEFAULT
+// ============================================
 app.configure(function(){
     app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
     app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
@@ -27,24 +28,36 @@ app.configure(function(){
     app.use(express.cookieParser());
     app.use(express.session({ 
         store: new MongoStore({
-            url: 'mongodb://localhost/mydb'
+            url: 'localhost/mydb'
         }),
-        secret: "fjdaksfjaklfjasklfadasdasdsadafdsfragrfgafdnvakl" }
-    ));
+        secret: "fjdaksfjaklfjasklfadasda"}));
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    require('./config/passport')(passport);
 });
 
-// development only
+
+// ============================================
+// APP CONFIGURATION - DEVELOPMENT
+// ============================================
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
     process.env.NODE_ENV = app.settings.env;
 }
 
-// test configuration
+
+// ============================================
+// APP CONFIGURATION - TEST
+// ============================================
 app.configure('test', function(){
     app.set('port', 3001);
 });
 
-// switch when run on openshift cloud
+
+// ============================================
+// OPENSHIFT CONFIGURATION
+// ============================================
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
@@ -54,10 +67,23 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
 }
 
 
-// Routes
+// ============================================
+// HELPERS
+// ============================================
+require('./apps/helpers')(app);
+
+
+// ============================================
+// ROUTES
+// ============================================
 require('./apps/read/routes')(app);
 require('./apps/query/routes')(app, request);
+require('./apps/authentication/routes')(app, passport);
 
+
+// ============================================
+// FIRE IT UP!
+// ============================================
 var server = app.listen(app.settings.port, app.settings.ipaddress, function() {
     console.log('%s || IP: %s || PORT: %d || ENV:',
     Date(Date.now() ), app.settings.ipaddress, app.settings.port, app.settings.env.toUpperCase());
