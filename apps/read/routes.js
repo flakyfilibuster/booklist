c435356c4a887fa6d005a7bad849a73668ee904b
flaky <flakyfilibuster@gmail.com>
2014-02-13 17:18:28 +0100

    again adding username to specific queries also checking for user object and displaying it on top of page now

--- apps/read/routes.js
+++ apps/read/routes.js
@@ -2,20 +2,23 @@ var Book = require('../../models/book');
 
 var routes = function(app, db){
     app.get('/', function(req, res){
-        Book.getAll(function(err, books) {
-            res.render(__dirname + "/views/index", {
-                amount: books.length,
-                title: " Completed Books",
-                books: books,
-                stylesheet1: "main",
-                stylesheet2: "bootstrap"
+        if(req.user) {
+            Book.getAll(req.user.username, function(err, books) {
+                res.render(__dirname + "/views/index", {
+                    amount: books.length,
+                    title: " Completed Books",
+                    books: books,
+                    user: req.user
+                });
             });
-        });
+        } else {
+            res.redirect('/login');
+        }
     });
 
     app.delete('/deletebook/:id', function(req, res){
-        Book.delete(req.params.id);
-        Book.getAll(function(err, books) {
+        Book.delete(req.user.username, req.params.id);
+        Book.getAll(req.user.username, function(err, books) {
             res.render(__dirname + "/views/_booktable", { books: books });
         });
     });
