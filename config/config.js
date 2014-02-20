ce963f7830d68058145eac4e4214d6c2424073f3
flaky <flakyfilibuster@gmail.com>
2014-02-20 18:16:25 +0100

    add googlebookapi string to config

--- config/config.js
+++ config/config.js
@@ -10,8 +10,9 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   process.env.OPENSHIFT_APP_NAME;
 }
 
-config.ipaddress    = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
-config.port         = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-config.secret       = process.env.OPENSHIFT_SECRET_TOKEN || "nvsudialb3242342vianmvo9hibakeykuabvsykadvnoifk907498732avikfbdv7489236432403afdkuav";
+config.ipaddress        = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
+config.port             = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+config.secret           = process.env.OPENSHIFT_SECRET_TOKEN || "nvsudialb3242342vianmvo9hibakeykuabvsykadvnoifk907498732avikfbdv7489236432403afdkuav";
+config.googlebooksapi   = 'https://www.googleapis.com/books/v1/volumes?q=isbn:';
 
 module.exports = config;
